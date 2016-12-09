c85dac76ec00060bc26d1346dfdc82a0acbcc672
karanmitroo <karan1.mitroo@gmail.com>
2016-12-09 14:10:56 +0530

    Added the first HTML to the website

--- mysite/settings.py
+++ mysite/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'jobsite')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
