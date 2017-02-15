d1c41e7532a98e0333ae68cfae0acd15c4cdc5ba
karanmitroo <karan1.mitroo@gmail.com>
2017-02-15 10:56:21 +0530

    Set the country to US for every result

--- jobsite/forms.py
+++ jobsite/forms.py
@@ -4,7 +4,7 @@ from .models import Job, Country
 class QueryForm(forms.Form):
     search = forms.CharField(required = True)
     location = forms.CharField(required = True)
-    country = forms.ModelChoiceField(queryset = Country.objects.all())
+    # country = forms.ModelChoiceField(queryset = Country.objects.all())
 
 class ContactForm(forms.Form):
 	full_name = forms.CharField(required = False)
