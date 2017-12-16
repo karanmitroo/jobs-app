0121fb9a1b212e6c147fc9183a0986323198bfc2
Karan Mitroo <karan1.mitroo@gmail.com>
2017-12-16 10:09:41 +0530

    Changed debug settings for production

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hzk4$-j_)((hwkuxxd_&==0$o3i@!+_%0jm%l6-ihiry(kksgm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
